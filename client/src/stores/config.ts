6758d6a7556eb463923dba9c833c8fbdde6cec59
= <joanpagarf09@gmail.com>
2025-12-11 08:19:53 -0600

    feat: configuration logic

---
+++ client/src/stores/config.ts
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia";
+import type { Configuration } from '@/types/Configuration';
+
+export const useConfigStore = defineStore('config', {
+  state: () => ({
+    config: null as Configuration | null,
+  }),
+  getters: {
+    getConfig: (state) => state.config,
+  },
+  actions: {
+    setConfig(config: Configuration) {
+      this.config = config;
+      localStorage.setItem('appConfig', JSON.stringify(config));
+    },
+
+    loadConfig() : Configuration | null {
+      const stored = localStorage.getItem('appConfig');
+      if (stored) {
+        try {
+            this.config = JSON.parse(stored) as Configuration;
+            return this.config;
+        } catch (error) {
+            console.error('Error al cargar la configuración:', error);
+            this.config = null;
+            return null;
+        }
+      }
+      return null;
+    },
+
+    clearConfig() {
+      this.config = null;
+      localStorage.removeItem('appConfig');
+    }
+  },
+});
---
+++ client/src/types/Configuration.ts
@@ -0,0 +1,4 @@
+export interface Configuration {
+    ip: string;
+    port: number;
+}
